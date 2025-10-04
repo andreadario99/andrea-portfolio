9087106d9cdb2904d683b0b0b3138300ef297111
Andrea Dario <andrea.dario@studio.unibo.it>
2025-10-04 11:51:37 +0200

    Add external component

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path"; // <-- add this
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/andrea-portfolio",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // <-- add this
+    },
+  },
 });
